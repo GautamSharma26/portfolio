ff40bb80d03288f68b28f3e466de5e10feb5939b
Gautam sharma <87752796+gautamkumar12345678@users.noreply.github.com>
2024-01-01 07:00:22 +0530

    added component and home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
 import Image from 'next/image'
+import imgDefault from "../public/images/Default.jpg"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <p>Hii</p>
+    <main className="grid md:grid-cols-3 sm:grid-cols-1 justify-center space-y-2 bg-slate-400 h-dvh p-12">
+      <div className='md:w-48 md:h-48 sm:w-12 sm:h-12 min-[200px]:h-16 bg-yellow-600 rounded-md hover:rounded-full relative'>
+        <div className='absolute md:h-48 sm:h-12 min-[200px]:h-16'>
+          <Image src={imgDefault} className='md:h-48 sm:h-24 min-[200px]:h-16'/>
+        </div>
+      </div>
+      <div className='md:w-48 md:h-48 bg-orange-900'>Second</div>
+      <div className='md:w-48 md:h-48 bg-green-900'>Second2</div>
     </main>
-  )
+  );
 }
---
+++ components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='absolute bottom-0'> Footer is here</div>
+  )
+}
+
+export default Footer;
