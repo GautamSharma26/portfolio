e1e81fc9c19c865b148f284dff24a9ba8c22b9ff
root354 <gautamk.inexture@gmail.com>
2024-01-05 19:37:31 +0530

    design some part of homepage

--- app/(main)/main/layout.tsx
+++ app/(main)/main/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainLayout = ({children}) => {
   return (
-    <div className='min-h-[calc(100vh-88px)]'>
+    <div className='min-h-[calc(100vh-144px)]'>
         {children}
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
 const RootPageMainD = () => {
   return (
     <div className="min-h-[calc(100vh-144px)]">
-      <h1>Demo</h1>
+      <div className="relative border-2 md:left-[42%] md:top-72 bg-green-300 h-64 w-64 rounded-full">Hii
+        <div className="absolute border-2 top-36 left-32 h-64 w-64 bg-yellow-200 rounded-full"></div>
+        <div className="absolute border-2 top-36 right-32 h-64 w-64 bg-red-400 rounded-full"></div>
+        <div className="absolute border-2 bottom-36 left-32 h-64 w-64 bg-orange-300 rounded-full"></div>
+        <div className="absolute border-2 bottom-36 right-32 h-64 w-64 bg-blue-300 rounded-full"></div>
+        
+      </div>
     </div>
   )
 }
