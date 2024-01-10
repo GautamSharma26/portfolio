da088ead7402a4ba8a0f1df1dbc5e8a67d3b2dcb
root354 <gautamk.inexture@gmail.com>
2024-01-10 20:35:04 +0530

    added page in main app

--- app/(main)/main/page.js
+++ app/(main)/main/page.js
@@ -1,7 +1,19 @@
+import Link from 'next/link'
 const MainPage = () => {
-    return (
-      <div>MainPage</div>
-    )
-  }
-  
-  export default MainPage;
+  return (
+    <div>
+      <div className='border-solid border-indigo-600 border-4 hover:border-dotted w-screen rounded-lg h-32'>
+        <p className='font-bold bg-red-500 w-16 text-center rounded-md'>Python</p>
+        <p className='relative left-8 font-bold bg-red-500 w-28 text-center rounded-md mt-2'>Frameworks</p>
+        <ul className="absolute list-disc flex flex-row left-16 space-x-8">
+          <li>Django</li>
+          <li>Django Rest Framework</li>
+          <li>FastAPI</li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default MainPage;
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 const RootPageMainD = () => {
   return (
     <div className="min-h-[calc(100vh-144px)] overflow-auto">
@@ -6,7 +7,8 @@ const RootPageMainD = () => {
         <div className='absolute md:top-20 md:left-16 md:w-16 sm:top-12 sm:left-6 animate-ping text-black font-bold'>
           Welcome
         </div>
-        <div className="absolute border-2 md:top-28 md:left-32 md:h-36 md:w-36 sm:h-20 sm:w-20 sm:top-20 sm:left-20 bg-yellow-200 rounded-full text-center p-2 break-word">Skill</div>
+        <Link href="/main">
+        <div className="absolute border-2 md:top-28 md:left-32 md:h-36 md:w-36 sm:h-20 sm:w-20 sm:top-20 sm:left-20 bg-yellow-200 rounded-full text-center p-2 break-word">Home</div></Link>
         <div className="absolute border-2 md:top-28 md:right-32 md:h-36 md:w-36 sm:h-20 sm:w-20 sm:top-20 sm:right-20 bg-red-400 rounded-full text-center p-2 break-words">Experience</div>
         <div className="absolute border-2 md:bottom-32 md:left-32 md:h-36 md:w-36 sm:h-20 sm:w-20 sm:bottom-20 sm:left-20 bg-orange-300 rounded-full text-center p-2">My Blogs</div>
         <div className="absolute border-2 md:bottom-32 md:right-32 md:h-36 md:w-36 sm:h-20 sm:w-20 sm:bottom-20 sm:right-20 bg-blue-300 rounded-full text-center p-2 hover:animate-spin break-words">Projects</div>
