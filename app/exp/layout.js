a8ab7030fc9cf588e732d4de0b53e79c012f426e
Gautam sharma <87752796+gautamkumar12345678@users.noreply.github.com>
2024-02-07 08:22:32 +0530

    added exp router

---
+++ app/exp/layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Explayout = ({children}) => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default Explayout
---
+++ app/exp/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Exppage</div>
+  )
+}
+
+export default page
