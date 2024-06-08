6d468ccec680855223f973a0a0002ce969a12c98
Gautam sharma <87752796+gautamkumar12345678@users.noreply.github.com>
2024-06-08 16:25:03 +0530

    changes in skills

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Page = () => {
   return (
     <div>
-    <h1 class="text-5xl text-white font-bold mb-8 animate-pulse">
+      <h1 class="text-5xl text-white font-bold mb-8 animate-pulse">
         Coming Soon
-    </h1>
-    <p class="text-white text-lg mb-8">
-        We're working hard to bring you something amazing. Stay tuned!
-    </p>
-</div>
+      </h1>
+      <p class="text-white text-lg mb-8">
+        We're working hard to bring you something amazing. Stay tuned!!
+      </p>
+      <div className="flex justify-center mt-20">
+        <Link href="/">
+          <button className="bg-blue-300 hover:bg-orange-300 text-white font-bold py-2 px-4 rounded-md transition duration-300 ease-in-out">
+            Home
+          </button>
+        </Link>
+      </div>
+    </div>
   )
 }
 export default Page
